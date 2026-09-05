873c73099106da97025032ce81d2c24ab8cba8fe
landontruax949 <landontruax949@gmail.com>
2026-09-05 00:24:35 +0000

    added space after first prompt

--- my-folder/Prog Assign_1 - Expressions.py
+++ my-folder/Prog Assign_1 - Expressions.py
@@ -1,4 +1,4 @@
-equation = int(input( "What kind of equation do you want to solve? (1 = addition, 2 = subtraction, 3 = multiplication, 4 = division)"))
+equation = int(input( "What kind of equation do you want to solve? (1 = addition, 2 = subtraction, 3 = multiplication, 4 = division) "))
 num1 = int(input("Enter the first number "))
 num2 = int(input("Enter the second number "))
 if equation == 1:
