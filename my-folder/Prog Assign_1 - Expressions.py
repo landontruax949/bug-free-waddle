62c9c39cb3160e030c7b394f65be0c97c23d09bf
landontruax949 <landontruax949@gmail.com>
2026-09-05 00:16:47 +0000

    test

---
+++ my-folder/Prog Assign_1 - Expressions.py
@@ -0,0 +1,12 @@
+equation = int(input( "What kind of equation do you want to solve? (1 = addition, 2 = subtraction, 3 = multiplication, 4 = division)"))
+num1 = int(input("Enter the first number "))
+num2 = int(input("Enter the second number "))
+if equation == 1:
+    total = num1 + num2
+elif equation == 2:
+    total = num1 - num2
+elif equation == 3:
+    total = num1 * num2
+elif equation == 4:
+    total = num1 / num2
+print("The total is ", total)
